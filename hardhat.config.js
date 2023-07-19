194ea9b14aaefec4d4376082749301e6963b2529
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-07-19 13:06:17 +0800

    finish

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,13 +14,10 @@ module.exports = {
       url:process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
       chainId: 5,
-      constructorArgs: [process.env.EmitWinner_Goerli_Address]
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
-      constructorArgs: [process.env.EmitWinner_Local_Address],
-      game1Address: [process.env.GAME1_CONTRACT_ADDRESS],
     }
   },
   solidity: "0.8.4",
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const colors = require('colors');
+const hre = require('hardhat');
+require('dotenv').config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log('deploying...'.yellow);
+  const signers = await hre.ethers.getSigners();
+  const deployer = signers[0];
+  
+  if(hre.network.name === 'localhost') {
+    console.log('Deploying to local network'.cyan);
+  } else {
+    console.log(`Deploying to ${hre.network.name}`.cyan);
+  }
+  const Token = await hre.ethers.getContractFactory("SquidwardTentaclesToken", deployer);
+  const token = await Token.deploy();
+  await token.waitForDeployment();
+  console.log(`Token deployed to: ${await token.getAddress()}`.green);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+main()
+.then(() => process.exit(0))
+.catch(error => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
