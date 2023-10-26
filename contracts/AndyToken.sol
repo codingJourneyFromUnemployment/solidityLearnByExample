aeb6423d695aa7e64b7878108281baab70044db7
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-10-26 23:51:53 +0800

    ERC721

--- contracts/AndyToken.sol
+++ contracts/AndyToken.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract AndyToken is ERC20 {
-  uint constant _initial_supply = 100000000000 * (10**18);
-  constructor() ERC20 ("AndyToken", "ANDY") {
-    mint(msg.sender, _initial_supply);
-  }
+contract AndyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+		uint constant _initial_supply = 10000000000 * (10**18);
+    constructor()
+        ERC20("AndyToken", "ANDY")
+        ERC20Permit("AndyToken")
+    {
+			_mint(msg.sender, _initial_supply);
+		}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
