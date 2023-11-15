caadf7b3874da1f9c972871a20d7a8be7791ed5e
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2023-11-15 22:27:30 +0800

    biDirectionalpayment

--- contracts/UniDirectionalPayment.sol
+++ contracts/UniDirectionalPayment.sol
@@ -25,8 +25,29 @@ contract UniDirectionalPaymentChannel is ReentrancyGuard {
     return keccak256(abi.encodePacked(address(this), _amount));
   }
 
-  function _getEthSignedHash(uint _amount) external view returns (bytes32){
+  function _getEthSignedHash(uint _amount) private view returns (bytes32){
     return _getHash(_amount).toEthSignedMessageHash();
+  }
+
+  function getEthSignedHash(uint _amount) external view returns (bytes32){
+    return _getEthSignedHash(_amount);
+  }
+
+  function _verify(uint _amount, bytes memory _sig) private view returns (bool){
+    return _getEthSignedHash(_amount).recover(_sig) == sender;
+  }
+
+  function verify(uint _amount, bytes memory _sig) external view returns (bool){
+    return _verify(_amount, _sig);
+  }
+
+  function close(uint _amount, bytes memory _sig) external nonReentrant {
+    require(msg.sender == receiver, "caller is not the receiver");
+    require(_verify(_amount, _sig), "invalid signature");
+
+    (bool sent, ) = receiver.call{value: _amount}("");
+    require(sent, "Failed to send Ether");
+    selfdestruct(sender);
   }
 }
 
